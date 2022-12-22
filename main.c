ec4efde1decd03affe23b00557f1b5525feb5dd9
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-12-22 18:20:40 +0100

    fix argc

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(__attribute__((unused)) int argc, char *argv[])
 		if (argc != 2)
 		{
 			fprintf(stderr, "USAGE: monty file\n");
-			exit(EXIT_FAILURE);
+			exit(1);
 		}
 		fprintf(stderr, "Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
