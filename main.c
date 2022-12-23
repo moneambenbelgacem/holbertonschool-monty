ca7bba720dc7f3f632d673ffcdb2b1a9c4b00ca8
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-12-23 16:12:21 +0100

    fix_comment

--- main.c
+++ main.c
@@ -22,13 +22,13 @@ int main(int argc, char *argv[])
 	var.stack_len = 0;
 	if (argc != 2)
 	{
-		dprintf(STDOUT_FILENO, "USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	fs = fopen(argv[1], "r");
 	if (fs == NULL)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	on_exit(free_lineptr, &lineptr);
