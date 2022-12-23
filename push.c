56352ba2d245b7ba563bcde3398ca9175687dc5d
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-12-23 16:14:16 +0100

    fix_comment_

--- push.c
+++ push.c
@@ -36,7 +36,7 @@ void m_push(stack_t **stack, unsigned int line_number)
 	arg = strtok(NULL, "\n\t\r ");
 	if (arg == NULL || check_for_digit(arg))
 	{
-		dprintf(STDOUT_FILENO,
+		fprintf(stderr,
 			"L%u: usage: push integer\n",
 			line_number);
 		exit(EXIT_FAILURE);
