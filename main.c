2cba18b7c6141bf14dd5b778cd9961ae4d67aa64
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-12-23 10:13:23 +0100

    fix free on exitt

--- main.c
+++ main.c
@@ -1,65 +1,34 @@
 #include "monty.h"
 
-int main(__attribute__((unused)) int argc, char *argv[])
+var_t varglob;
+
+int main( int argc, char *argv[])
 {
 
 	stack_t *stack = NULL;
-	char *filename;
-	FILE *fp;
-	char line[MAX_LINE_LENGTH];
-	char *opcode, *arg;
-	int value = 0, line_number = 1;
+	unsigned int line_count = 0;
+	FILE *fp = NULL;
+	char *lineptr = NULL, *op = NULL;
+	size_t n = 0;
 
-	filename = argv[1];
-	fp = fopen(filename, "r");
-	if ((fp == NULL))
+	varglob.queue = 0;
+	varglob.stack_len = 0;
+
+	if(argc != 2)
 	{
-		if (argc != 2)
-		{
-			fprintf(stderr, "USAGE: monty file\n");
-			exit(EXIT_FAILURE);
-		}
-		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		dprintf(STDOUT_FILENO, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	while ((fgets(line, MAX_LINE_LENGTH, fp) != NULL)&& line_number++)
+	fp = fopen(argv[1],"r");
+	if (fp == NULL)
 	{
-		if (strlen(line) == 1)
-		{
-			line_number++;
-			continue;
-		}
-		else
-		{
-
-			opcode = strtok(line, " \n\t\r");
-			if (strcmp(opcode, "push") == 0)
-			{
-				arg = strtok(NULL, " \n\t\r");
-				if (arg == NULL)
-				{
-					fprintf(stderr, "L%d: usage: push integer\n", line_number);
-
-				}
-				value = atoi(arg);
-			
-				push(&stack, value);
-			}
-			else if (strcmp(opcode, "pall") == 0)
-			{
-				pall(stack);
-			}
-			else
-			{
-
-				fprintf(stderr, "L%d: unknown instruction pushe\n",line_number);
-				exit(EXIT_FAILURE);
-			}
-		}
-		line_number++;
+		dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
-	fclose(fp);
 	
+	on_exit(free_lineptr, &lineptr);
+	on_exit(free_stack, &stack);
+	on_exit(m_fs_close, fp);
 	return (0);
 }
