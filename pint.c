5d71c704def3b8f58506441121a42a07e2981f65
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2023-01-07 10:58:56 +0100

    print integer

--- pint.c
+++ pint.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * m_pint - print value on top of `stack', or exit if stack is empty
+ * m_pint -   print value on top of `stack', or exit if stack is empty
  * @stack: double pointer to head of stack
  * @line_number: line number of current operation
  *
